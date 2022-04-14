163568df0bf712632aa3d2f984e92f48745de7a2
yanjiyi <630758569@qq.com>
2022-04-14 13:42:17 +0800

    20220414 almost complete

--- src/main/java/com/suannai/netdisk/controller/FriendController.java
+++ src/main/java/com/suannai/netdisk/controller/FriendController.java
@@ -1,6 +1,7 @@
 package com.suannai.netdisk.controller;
 
 import com.suannai.netdisk.dao.Message;
+import com.suannai.netdisk.model.Friend;
 import com.suannai.netdisk.model.SysConfig;
 import com.suannai.netdisk.model.Task;
 import com.suannai.netdisk.model.User;
@@ -126,7 +127,31 @@ public class FriendController {
                                     message.setErrorMsg("不是该用户事务！拒绝访问！");
                                     message.setStatusCode(5300);
                                 }else {
+                                    User friUser = userService.QueryByID(task.getTargetid());
+                                    if(friUser==null)
+                                    {
 
+                                    }
+                                    Friend friend = new Friend();
+                                    friend.setDate(new Date());
+                                    friend.setFrdid(task.getTargetid());
+                                    friend.setOwnner(user.getId());
+                                    friend.setWho(friUser.getUsername());
+
+                                    boolean status = friendService.addFriend(friend);
+
+                                    friend.setOwnner(task.getTargetid());
+                                    friend.setFrdid(user.getId());
+                                    friend.setWho(user.getUsername());
+
+                                    if(status && friendService.addFriend(friend))
+                                    {
+                                        message.setStatusCode(2000);
+                                        message.setErrorMsg("操作成功！");
+                                    }else {
+                                        message.setStatusCode(5700);
+                                        message.setErrorMsg("操作失败！");
+                                    }
                                 }
                             }
                         }else {
@@ -141,6 +166,62 @@ public class FriendController {
 
             message.setStatusCode(5500);
             message.setErrorMsg("已被管理员禁止接受好友功能！");
+        }
+
+        return message;
+    }
+
+    @RequestMapping(value = "/refuseFriend")
+    public Message refuseFriend(@RequestParam("taskid") int taskid,HttpSession session,HttpServletResponse response) throws IOException {
+        Message message = new Message();
+
+        List<SysConfig> sysConfigs = sysConfigService.GetSysConfig();
+        if(sysConfigs.isEmpty())
+        {
+            message.setErrorMsg("已被管理员禁止拒绝好友！");
+            message.setStatusCode(5500);
+        }else {
+            for(SysConfig sysConfig : sysConfigs)
+            {
+                if(sysConfig.getName().equals("AllowRefuseFriend")&&sysConfig.getValue().equals("YES"))
+                {
+                    User user = (User) session.getAttribute("user");
+                    if(user!=null)
+                    {
+                        Task task = taskService.queryByID(taskid);
+                        if(task!=null)
+                        {
+                            int friType = taskTypeService.GetTaskID("Friend");
+                            if(friType>0)
+                            {
+                                if(friType == task.getTasktype())
+                                {
+                                    if(taskService.lockTask(task))
+                                    {
+                                        message.setErrorMsg("拒绝成功！");
+                                        message.setStatusCode(2000);
+                                    }else {
+                                        message.setStatusCode(5400);
+                                        message.setErrorMsg("拒绝失败！");
+                                    }
+                                }else{
+                                    message.setStatusCode(5200);
+                                    message.setErrorMsg("事务类型不匹配！");
+                                }
+                            }else{
+                                message.setErrorMsg("无效事务类型！");
+                                message.setStatusCode(5200);
+                            }
+                        }else {
+                            message.setStatusCode(5100);
+                            message.setErrorMsg("无效事务ID");
+                        }
+                    }else response.sendRedirect("/index.html");
+                }
+            }
+
+            message.setErrorMsg("已被管理员禁止拒绝好友！");
+            message.setStatusCode(5500);
         }
 
         return message;
