aafc8bf23eccced353ef89d8bfff3db9540700af
yanjiyi <630758569@qq.com>
2022-04-14 17:13:20 +0800

    20220414 almost complete aria2Utils dev

--- src/main/java/com/suannai/netdisk/utils/Aria2Utils.java
+++ src/main/java/com/suannai/netdisk/utils/Aria2Utils.java
@@ -1,6 +1,7 @@
 package com.suannai.netdisk.utils;
 
 import com.suannai.netdisk.dao.AddUriOption;
+import org.apache.ibatis.reflection.wrapper.BaseWrapper;
 
 import java.awt.image.AreaAveragingScaleFilter;
 import java.io.IOException;
@@ -16,13 +17,13 @@ public class Aria2Utils {
         return aria2Command.send(jsonRpcURL);
     }
 
-    public static String addTorrent(String jsonRpcURL,String whereTorrentFile) throws IOException {
+    public static String addTorrent(String jsonRpcURL,String whereTorrentFile,AddUriOption option) throws IOException {
         byte[] b = Files.readAllBytes(Paths.get(whereTorrentFile));
         String torrent = Base64.getEncoder().encodeToString(b);
 
         Aria2Command aria2Command = new Aria2Command();
         aria2Command.setMethod(aria2Command.METHOD_ADD_TORRENT);
-        aria2Command.addParam(new String[]{torrent});
+        aria2Command.addParam(new String[]{torrent}).addParam(option);
         return aria2Command.send(jsonRpcURL);
     }
 
@@ -89,5 +90,121 @@ public class Aria2Utils {
         return aria2Command.send(jsonRpcURL);
     }
 
+    public static String tellStatus(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLSTATUS);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellStatus(String jsonRpcURL,String gid,String[] whatKeyUWant) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLSTATUS);
+        aria2Command.addParam(gid).addParam(whatKeyUWant);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getUris(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETURIS);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getFiles(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETFILES);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getPeers(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETPEERS);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getServers(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETSERVERS);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellActive(String jsonRpcURL) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLACTIVE);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellActive(String jsonRpcURL,String[] whatKeyUWant) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLACTIVE);
+        aria2Command.addParam(whatKeyUWant);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellWatiting(String jsonRpcURL,int offset,int num) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLWAITING);
+        aria2Command.addParam(offset).addParam(num);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellWatiting(String jsonRpcURL,int offset,int num,String[] whatKeyUWant) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLWAITING);
+        aria2Command.addParam(offset).addParam(num).addParam(whatKeyUWant);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellStopped(String jsonRpcURL,int offset,int num) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLSTOPPED);
+        aria2Command.addParam(offset).addParam(num);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String tellStopped(String jsonRpcURL,int offset,int num,String[] whatKeyUWant) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_TELLSTOPPED);
+        aria2Command.addParam(offset).addParam(num).addParam(whatKeyUWant);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String changePosition(String jsonRpcURL,String gid,int pos,String how) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_CHANGEPOSITION);
+        aria2Command.addParam(gid).addParam(pos).addParam(how);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String changeUri(String jsonRpcURL,String gid,int fileindex,String[] delUris,String[] addUris) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_CHANGEURI);
+        aria2Command.addParam(gid).addParam(fileindex).addParam(delUris).addParam(addUris);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String changeUri(String jsonRpcURL,String gid,int fileindex,String[] delUris,String[] addUris,int position) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_CHANGEURI);
+        aria2Command.addParam(gid).addParam(fileindex).addParam(delUris).addParam(addUris).addParam(position);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getOption(String jsonRpcURL,String gid) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETOPTION);
+        aria2Command.addParam(gid);
+        return aria2Command.send(jsonRpcURL);
+    }
+
+    public static String getGlobalStat(String jsonRpcURL) throws IOException {
+        Aria2Command aria2Command = new Aria2Command();
+        aria2Command.setMethod(aria2Command.METHOD_GETGLOBALSTAT);
+        return aria2Command.send(jsonRpcURL);
+    }
 
 }
