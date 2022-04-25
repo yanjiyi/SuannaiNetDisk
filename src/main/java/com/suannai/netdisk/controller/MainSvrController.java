158cca23ecd47e3781225d564252aaa015a7a78a
yanjiyi <630758569@qq.com>
2022-04-25 13:42:18 +0800

    20220425 fix

--- src/main/java/com/suannai/netdisk/controller/MainSvrController.java
+++ src/main/java/com/suannai/netdisk/controller/MainSvrController.java
@@ -557,13 +557,13 @@ public class MainSvrController {
     }
 
     @RequestMapping(value = "/api/querySysFileTab")
-    public SysFileTab querySysFileTab(HttpServletRequest request,HttpSession session,HttpServletResponse response) throws IOException {
+    public SysFileTab querySysFileTab(@RequestParam("id") int id,HttpSession session,HttpServletResponse response) throws IOException {
         if(sysConfigService.ConfigIsAllow("AllowQuerySysFileTab"))
         {
             User user = (User) session.getAttribute("user");
             if(user!=null)
             {
-                return sysFileTabService.queryByID(Integer.parseInt(request.getParameter("id")));
+                return sysFileTabService.queryByID(id);
             }else response.sendRedirect("/index.html");
         }
 
@@ -571,13 +571,13 @@ public class MainSvrController {
     }
 
     @RequestMapping(value = "/api/queryService")
-    public Service queryService(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
+    public Service queryService(@RequestParam("id") int id, HttpServletResponse response, HttpSession session) throws IOException {
         if(sysConfigService.ConfigIsAllow("AllowQueryService"))
         {
             User user = (User) session.getAttribute("user");
             if(user!=null)
             {
-                Service result = mainSvrService.queryByID(Integer.parseInt(request.getParameter("id")));
+                Service result = mainSvrService.queryByID(id);
                 if(result!=null)
                 {
                     if(Objects.equals(result.getUserid(), user.getId()))
